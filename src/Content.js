150e413bb3bd0c7fca1d2f2fa1d1aa7a5450640e
Fentahun-arch <fentahuna1221@gmail.com>
2022-03-15 11:37:07 +0300

    commitinng changes

--- src/Content.js
+++ src/Content.js
@@ -1,15 +1,23 @@
-import React  from 'react'
-
+import React, { useState } from 'react';
 function Content() {
-  
+  const [name, setName] = useState('Fantish');
+  const [count, setCount] = useState(0);
   const handleNamechange = () => {
     const names = ['Abebe', 'Fentahun', 'Solomon'];
     const int = Math.floor(Math.random() * 3);//if we use 2 solomon is not displayed or we use 4 hello and all the name displayed randomly.
-    return names[int];
+    setName(names[int]);
   } 
   const handleClick = () => {
     console.log('You clicked it');
+    console.log(count);
   }
+  const handleClick0 = () => {
+    setCount(count+1);
+    setCount(count +1);
+
+    console.log(count);
+  }
+
   const handleClick2 =(name) => {
     console.log(name,'was clicked');
   }
@@ -18,10 +26,14 @@ function Content() {
   }
   return  ( 
       <main>
-      <p>Hello {handleNamechange()}</p>
-      <button onClick={ handleClick }>Click it</button>
+          <p onDoubleClick={handleClick}>
+                  Hello {name}
+          </p>
+      <button onClick={ handleNamechange }>Change Name</button>
             <button onClick={()=> handleClick2('Fentish')}>Click italso</button> 
-            <button onClick={(e)=> handleClick3(e)}>Click3</button>
+      <button onClick={ (e) => handleClick3(e) }>Click3</button>
+      <button onClick={handleClick0}>Click0</button> 
+
 
       </main>
   )
